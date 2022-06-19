8705f80065a5d97a852b49861adb0d0875294673
Andrey Kovalevskij <andrey.kovalevskij@gmail.com>
2022-06-19 11:39:18 +0300

    solve CORS error

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
-const express = require("express");
-require("dotenv").config();
-const colors = require("colors");
-const { graphqlHTTP } = require("express-graphql");
-const schema = require("./schema/schema");
-const connectDB = require("./config/db");
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const colors = require('colors');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -11,11 +12,13 @@ const app = express();
 // Connect to database
 connectDB();
 
+app.use(cors());
+
 app.use(
-  "/graphql",
+  '/graphql',
   graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV === "development",
+    graphiql: process.env.NODE_ENV === 'development',
   })
 );
 
